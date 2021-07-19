214fb848e53ca7577d4e069aa07154284edb8057
dromero-dotcom <hispanouk@yahoo.co.uk>
2021-07-19 19:44:29 +0100

    version 2.2 not tested

--- actions/PayGapRetriever3.py
+++ actions/PayGapRetriever3.py
@@ -20,7 +20,9 @@ import csv
 def get_the_time():
     #current_time = f"{dt.datetime.now()}"
     current_time =  dt.datetime.now()
-    time_str = "It's " + current_time.strftime("%I:%M %p")
+    time_str = "It's " + current_time.strftime("%#I:%M %p")
+    #time_str = f'{current_time:%A} {current_time:%B} {current_time.day}, {current_time.year}'
+    #time_str = f'{current_time:%I} {current_time:%M} {current_time.hour}, {current_time.minute}'
     #print(current_time.strftime("%A, %I:%M %p"))
     return (time_str)
 
@@ -36,8 +38,9 @@ def get_gpg_intl(ctry_name):
             gpg_dict[key] = gpg_val
 
     report_yr = '2019'
-    gpg_result = gpg_dict[ctry_name]
-    reply = "{}{}{}{}{}{}{}".format("The avg. gender pay gap in ", report_yr, " in ", ctry_name, " was ", gpg_result, "%")
+    ctry_name_folded = ctry_name.casefold()
+    gpg_result = gpg_dict[ctry_name_folded]
+    reply = "{}{}{}{}{}{}{}".format("The avg. gender pay gap in ", report_yr, " in ", ctry_name.title(), " was ", gpg_result, "%")
     #print(reply)
     return(reply)
 
@@ -53,14 +56,28 @@ def get_gpg_uk(company):
             gpg_dict[key] = gpg_val
 
     report_yr = '2020'
-    gpg_result = gpg_dict[company]
-    reply = "{}{}{}{}{}{}{}".format("The avg. gender pay gap in ", report_yr, " at ", company, " was ", gpg_result, "%")
+    try:
+        company_folded = company.casefold()
+        gpg_result = gpg_dict[company_folded]
+    except KeyError:
+        reply = "{}{}".format("I'm sorry, I don't have gender pay gap information for ", company)
+    else:
+        reply = "{}{}{}{}{}{}{}".format("The avg. gender pay gap in ", report_yr, " at ", company, " was ", gpg_result, "%")
+
     return(reply)
 
 
 if __name__ == '__main__':
+    Link1 = "[MoneyHelper.org.uk](https://www.moneyhelper.org.uk/en/everyday-money/budgeting/budget-planner)"
+    Link2 = "[MoneySavingExpert.com](https://www.moneysavingexpert.com/banking/budget-planning/#bplanner)"
+    Link3 = "[Investopedia.com](https://www.investopedia.com/budgeting-and-savings-4427755)"
+    msg = "{}{}".format("The links to the tools are: [MoneyHelper.org.uk]", Link1)
+    msg1 = "These websites can help with tips and tools for personal budgeting: "
+    msg2 = "What would you like to see next?"
+    reply= msg1+Link1+", "+Link2+", "+Link3+ ". "+msg2
     print("GPG for a country:")
-    print(get_gpg_intl('USA'))
+    print(get_gpg_intl('mexico'))
     print("GPG for a UK Company:")
-    print(get_gpg_uk('Google'))
+    print(get_gpg_uk('B&Q'))
     print(get_the_time())
+    #print (reply)
--- actions/actions.py
+++ actions/actions.py
@@ -1,37 +1,25 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
+# This file contains custom actions for the chatbot ABIE.
+# It runs custom Python code in conjunction with Rasa Open Source.
 #
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+# Feminist Chatbot ABIE, Version 2.1
+# Created by David Romero, MSc Student- University of Warwick
+# July 2021.
 
 from typing import Any, Text, Dict, List
-
 from rasa_sdk.events import SlotSet, AllSlotsReset
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-
-#import pandas as pd
 import datetime as dt
 import csv
 
+# Data files with Gender Pay Gap statistics: countries and UK companies
 GPG_INTL_PATH = "actions/gpg_intl.csv"
 GPG_UK_PATH = "actions/gpg_uk.csv"
 
-class ActionHelloWorld(Action):
-
-     def name(self) -> Text:
-         return "action_hello_world"
-
-     def run(self, dispatcher: CollectingDispatcher,
-             tracker: Tracker,
-             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-         dispatcher.utter_message(text="I'm very glad to hear that!")
-
-         return []
-
 class ActionGetTime(Action):
-
+# This class obtains the current time and sends back a message to the user,
+# displaying hh:mm am/pm formatting.   
+    
      def name(self) -> Text:
          return "action_show_time"
 
@@ -39,13 +27,16 @@ class ActionGetTime(Action):
              tracker: Tracker,
              domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
          current_time =  dt.datetime.now()
-         reply = "It's " + current_time.strftime("%I:%M %p")
+         reply = "Where I am it's " + current_time.strftime("%#I:%M %p")
          dispatcher.utter_message(text = reply)
 
          return []
 
 class ActionGetGPGIntl(Action):
-
+# Class that queries a separate text file with gender pay gap statistics for
+# various countries: Western Europe, North America, a couple in South America
+# and some in Asia Pacific incl. AU, NZ, SG, KR and JP.
+    
      def name(self) -> Text:
          return "action_get_gpg_intl"
      
@@ -57,38 +48,31 @@ class ActionGetGPGIntl(Action):
              domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
                   
          ctry_name = tracker.get_slot('country')
-         #ctry_name = tracker.latest_message['text']
-         gpg_dict = {}
-         """
-         gpg_dict['Austria']= '20'
-         gpg_dict['Germany']= '19'
-         gpg_dict['Mexico']= '18.8'
-         gpg_dict['USA']= '18.5'
-         """
-         
+         gpg_dict = {}         
          with open(self.gpg_file) as f:
              reader = csv.reader(f)
-             #header_row = next(reader)
              for row in reader:
                  key = row[0]
                  gpg_val = row[1]
                  gpg_dict[key] = gpg_val
                  
-         #ctry_name = 'Germany'
-         report_yr = '2019'
+         report_yr = '2019' # Latest statistics available in July 2021.
          try:
-             gpg_result = gpg_dict[ctry_name]
+             ctry_name_folded = ctry_name.casefold()
+             gpg_result = gpg_dict[ctry_name_folded]
          except KeyError:
              reply = "{}{}".format("I'm sorry, I don't have gender pay gap information for ", ctry_name)
          else:
-             reply = "{}{}{}{}{}{}{}".format("The avg. gender pay gap in ", report_yr, " in ", ctry_name, " was ", gpg_result, "%")
+             reply = "{}{}{}{}{}{}{}".format("The avg. gender pay gap in ", report_yr, " in ", ctry_name.title(), " was ", gpg_result, "%")
          
-         dispatcher.utter_message(text = reply)
-
-         #return [SlotSet("country", ctry_name2)]
-         return [AllSlotsReset()]
+         dispatcher.utter_message(text = reply)         
+         # Need to reset all slots in case user wants to see a different ctry.
+         return [AllSlotsReset()] 
 
 class ActionGetGPGUK(Action):
+# Class that queries separate text file with gender pay gap statistics for
+# several UK-based companies that are legally required to report gpg.
+# Latest complete report from uk.gov.uk includes 2020.
 
      def name(self) -> Text:
          return "action_get_gpg_uk"
@@ -102,38 +86,31 @@ class ActionGetGPGUK(Action):
              domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
          company = tracker.get_slot('company')
-         #company = tracker.latest_message['text']
          gpg_dict = {}
-         """
-         gpg_dict['Google UK']= '17'
-         gpg_dict['HSBC']= '33'
-         gpg_dict['Tesco']= '9.7'
-         """
-         #gpg_file = 'gpg_uk.csv'
          with open(self.gpg_file) as f:
              reader = csv.reader(f)
-             #header_row = next(reader)
              for row in reader:
                  key = row[0]
                  gpg_val = row[1]
                  gpg_dict[key] = gpg_val
                  
-         #company = 'Google UK'
-         report_yr = '2020'
+         report_yr = '2020' # Latest statistics available in July 2021.
          try:
-             gpg_result = gpg_dict[company]
+             company_folded = company.casefold()
+             gpg_result = gpg_dict[company_folded]
          except KeyError:
              reply = "{}{}".format("I'm sorry, I don't have gender pay gap information for ", company)
          else:
              reply = "{}{}{}{}{}{}{}".format("The avg. gender pay gap in ", report_yr, " at ", company, " was ", gpg_result, "%")
 
          dispatcher.utter_message(text = reply)
-
-         #return [SlotSet("company", company2)]
+         # Need to reset all slots in case user wants to see a different company.
          return [AllSlotsReset()]
      
 class ActionReceiveName(Action):
-
+# This class supports a custom action to receive the name of the user.
+# It populates the slot 'name' and keeps track of it during the conversation.
+    
     def name(self) -> Text:
         return "action_receive_name"
 
@@ -147,6 +124,8 @@ class ActionReceiveName(Action):
 
 
 class ActionSayName(Action):
+# This class supports a custom action to say or repeat the name of the user.
+# If the slot 'name' is empty the bot doesn't know and replies accordingly.
 
     def name(self) -> Text:
         return "action_say_name"
